6dff5002312986680a7e9af81517fd1aedcdbf4d
Kamyar <kamyarkarimi98@gmail.com>
2021-12-13 00:22:58 -0500

    Video list and items initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
-    onTermSubmit = (term) => {
-        console.log(term);
+    state = { videos: [] };
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search',{
+            params: {
+                q: term
+            }
+        });
+
+        this.setState({ videos: response.data.items });
     };
 
     render() {
         return(
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                {/* <h3>I have {this.state.videos.length} videos </h3> */}
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     };
