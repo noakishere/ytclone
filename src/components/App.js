d7c6a9efa893b070b9fe7c48232869401a4edf95
Kamyar <kamyarkarimi98@gmail.com>
2021-12-18 21:22:55 -0500

    Grid view and video player implemented

--- src/components/App.js
+++ src/components/App.js
@@ -5,30 +5,49 @@ import youtube from "../apis/youtube";
 import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-    state = { videos: [], selectedVideo: null };
-    onTermSubmit = async (term) => {
-        const response = await youtube.get('/search',{
-            params: {
-                q: term
-            }
-        });
+	state = { videos: [], selectedVideo: null };
 
-        this.setState({ videos: response.data.items });
-    };
+	// componentDidMount() {
+	// 	this.onTermSubmit("buildings");
+	// }
 
-    onVideoSelect = (video) => {
-        this.setState({ selectedVideo: video });
-    };
+	onTermSubmit = async (term) => {
+		const response = await youtube.get("/search", {
+			params: {
+				q: term,
+			},
+		});
 
-    render() {
-        return(
-            <div className="ui container">
-                <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
-            </div>
-        );
-    };
+		this.setState({
+			videos: response.data.items,
+			selectedVideo: response.data.items[0],
+		});
+	};
+
+	onVideoSelect = (video) => {
+		this.setState({ selectedVideo: video });
+	};
+
+	render() {
+		return (
+			<div className="ui container">
+				<SearchBar onFormSubmit={this.onTermSubmit} />
+				<div className="ui grid">
+					<div className="ui row">
+						<div className="eleven wide column">
+							<VideoDetail video={this.state.selectedVideo} />
+						</div>
+						<div className="five wide column">
+							<VideoList
+								onVideoSelect={this.onVideoSelect}
+								videos={this.state.videos}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,18 +1,30 @@
 import React from "react";
 
 const VideoDetail = ({ video }) => {
-    if (!video) {
-        return <div>Loading...</div>
-    }
+	if (!video) {
+		return (
+			<div className="ui segment">
+				<p></p>
+				<div className="ui active inverted dimmer">
+					<div className="ui loader"></div>
+				</div>
+			</div>
+		);
+	}
 
-    return (
-        <div>
-            <div className="ui segment">
-                <h4 className="ui header">{video.snippet.title}</h4>
-                <p>{video.snippet.description}</p>
-            </div>
-        </div>
-    );
-}
+	const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+	return (
+		<div>
+			<div className="ui embed">
+				<iframe title="Video player" src={videoSrc} />
+			</div>
+			<div className="ui segment">
+				<h4 className="ui header">{video.snippet.title}</h4>
+				<p>{video.snippet.description}</p>
+			</div>
+		</div>
+	);
+};
 
 export default VideoDetail;
